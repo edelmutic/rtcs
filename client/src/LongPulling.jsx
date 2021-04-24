27074dff17faa3a8fe66b2c7a3c19f7bcf1ef3bb
edelmutic <edelmutic@gmail.com>
2021-04-24 15:23:23 +0300

    add effect hook and subscribe

--- client/src/LongPulling.jsx
+++ client/src/LongPulling.jsx
@@ -6,9 +6,21 @@ const LongPulling = () => {
     const [value, setValue] = useState('');
 
 
+    useEffect(() => {
+        subscribe()
+    }, [])
 
-
-
+    const subscribe = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:5000/get-messages')
+            setMessages(prev => [data, ...prev])
+            await subscribe()
+        } catch (e) {
+            setTimeout(() => {
+                subscribe()
+            }, 500)
+        }
+    }
 
     const sendMessage = async () => {
         await axios.post('http://localhost:5000/new-messages', {
@@ -22,7 +34,7 @@ const LongPulling = () => {
             <div>
                 <div className="form">
                     <input value={value} onChange={e => setValue(e.target.value)} type="text" />
-                    <button onClick={sendMessage}>Отправить</button>
+                    <button onClick={sendMessage}>Send</button>
                 </div>
                 <div className="messages">
                     {messages.map(mess =>
