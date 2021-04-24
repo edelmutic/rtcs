4ded054af1bf38bf5c10eee8607dc37a5d67be07
edelmutic <edelmutic@gmail.com>
2021-04-24 15:16:38 +0300

    little fixes

--- client/src/LongPulling.jsx
+++ client/src/LongPulling.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const LongPulling = () => {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
+
+
+
+
+
+
+    const sendMessage = async () => {
+        await axios.post('http://localhost:5000/new-messages', {
+            message: value,
+            id: Date.now()
+        })
+    }
+
     return (
-        <div className='center'>
+        <div className="center">
             <div>
                 <div className="form">
-                    <input type="text" />
-                    <button>Send</button>
+                    <input value={value} onChange={e => setValue(e.target.value)} type="text" />
+                    <button onClick={sendMessage}>Отправить</button>
+                </div>
+                <div className="messages">
+                    {messages.map(mess =>
+                        <div className="message" key={mess.id}>
+                            {mess.message}
+                        </div>
+                    )}
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default LongPulling
+export default LongPulling;
