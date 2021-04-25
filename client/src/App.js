7e6881bd17657fa10a75704b26ea3aed281454ea
edelmutic <edelmutic@gmail.com>
2021-04-25 12:20:57 +0300

    add ws component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import EventSourcing from './EventSourcing';
 import LongPulling from './LongPulling';
+import WebSocket from './WebSocket';
 
 function App() {
   return (
     <div>
-      <LongPulling />
-      <EventSourcing />
+      <WebSocket />
     </div>
   );
 }
